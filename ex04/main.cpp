b50f86b1251c47c7c41519ff95766d97d1183d82
Batrii <dhj.nafiai@gmail.com>
2025-10-02 16:55:10 +0100

    some new exercices , and edit in codes

---
+++ ex04/main.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
+    std::ifstream infile(argv[1]);
+    std::string line;
+    
+    std::string filename = argv[1] + std::string(".replace");
+    std::ofstream outfile(filename);
+    if (!infile.is_open() || !outfile.is_open())
+    {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    while (std::getline(infile, line))
+    {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+            pos += s2.length();
+        }
+        outfile << line << std::endl;
+    }
+    return 0;
+}
